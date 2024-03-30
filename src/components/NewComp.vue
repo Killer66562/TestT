46fe03e75b2e501f5ea5fa53d7d9171782c83af0
rookie <a2uuix@gmail.com>
2024-03-30 13:58:10 +0800

    Comment

--- src/components/NewComp.vue
+++ src/components/NewComp.vue
@@ -2,7 +2,10 @@
 import { ref } from 'vue';
 const num = ref(0);
 const addOne = () => {
-    num.value++;
+    num.value += 5;
+}
+const zero =() => {
+    num.value=0;
 }
 </script>
 
@@ -11,6 +14,7 @@ const addOne = () => {
     <h2>這是副標</h2>
     <h3>Value: {{ num }}</h3>
     <button type="button" @click="addOne">Add 1</button>
+    <button type="button" @click="zero">zero</button>
 </template>
 
 <style></style>
