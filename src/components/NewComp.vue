ca335f0ce776a24c3d8c9dc867be021d40a9b465
Jimmy688382 <jimmy688384@gmail.com>
2024-03-30 13:40:14 +0800

    Init

---
+++ src/components/NewComp.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+const num = ref(0);
+const addOne = () => {
+    num.value++;
+}
+</script>
+
+<template>
+    <h1>這是標題</h1>
+    <h2>這是副標</h2>
+    <h3>Value: {{ num }}</h3>
+    <button type="button" @click="addOne">Add 1</button>
+</template>
+
+<style></style>
